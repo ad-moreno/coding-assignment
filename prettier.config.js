a86ccffd56993f9d971cacc9507ac95c54f09873
Adrian Moreno <ad.moreno@am-im.com>
2024-03-20 20:35:48 +0100

    Add prettier and enforce styling

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  printWidth: 120,
+  singleQuote: true,
+  bracketSpacing: false,
+  trailingComma: 'es5',
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  proseWrap: 'preserve',
+  semi: true,
+};
--- src/App.js
+++ src/App.js
@@ -1,78 +1,74 @@
-import { useEffect, useState } from 'react'
-import { Routes, Route, createSearchParams, useSearchParams, useNavigate } from "react-router-dom"
-import { useDispatch, useSelector } from 'react-redux'
-import 'reactjs-popup/dist/index.css'
-import { fetchMovies } from './data/moviesSlice'
-import { ENDPOINT_SEARCH, ENDPOINT_DISCOVER, ENDPOINT, API_KEY } from './constants'
-import Header from './components/Header'
-import Movies from './components/Movies'
-import Starred from './components/Starred'
-import WatchLater from './components/WatchLater'
-import YouTubePlayer from './components/YoutubePlayer'
-import './app.scss'
+import {useEffect, useState} from 'react';
+import {Routes, Route, createSearchParams, useSearchParams, useNavigate} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import 'reactjs-popup/dist/index.css';
+import {fetchMovies} from './data/moviesSlice';
+import {ENDPOINT_SEARCH, ENDPOINT_DISCOVER, ENDPOINT, API_KEY} from './constants';
+import Header from './components/Header';
+import Movies from './components/Movies';
+import Starred from './components/Starred';
+import WatchLater from './components/WatchLater';
+import YouTubePlayer from './components/YoutubePlayer';
+import './app.scss';
 
 const App = () => {
+  const state = useSelector(state => state);
+  const {movies} = state;
+  const dispatch = useDispatch();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const searchQuery = searchParams.get('search');
+  const [videoKey, setVideoKey] = useState();
+  const [isOpen, setOpen] = useState(false);
+  const navigate = useNavigate();
 
-  const state = useSelector((state) => state)
-  const { movies } = state  
-  const dispatch = useDispatch()
-  const [searchParams, setSearchParams] = useSearchParams()
-  const searchQuery = searchParams.get('search')
-  const [videoKey, setVideoKey] = useState()
-  const [isOpen, setOpen] = useState(false)
-  const navigate = useNavigate()
-  
-  const closeModal = () => setOpen(false)
-  
-  const closeCard = () => {
+  const closeModal = () => setOpen(false);
 
-  }
+  const closeCard = () => {};
 
-  const getSearchResults = (query) => {
+  const getSearchResults = query => {
     if (query !== '') {
-      dispatch(fetchMovies(`${ENDPOINT_SEARCH}&query=`+query))
-      setSearchParams(createSearchParams({ search: query }))
+      dispatch(fetchMovies(`${ENDPOINT_SEARCH}&query=` + query));
+      setSearchParams(createSearchParams({search: query}));
     } else {
-      dispatch(fetchMovies(ENDPOINT_DISCOVER))
-      setSearchParams()
+      dispatch(fetchMovies(ENDPOINT_DISCOVER));
+      setSearchParams();
     }
-  }
+  };
 
-  const searchMovies = (query) => {
-    navigate('/')
-    getSearchResults(query)
-  }
+  const searchMovies = query => {
+    navigate('/');
+    getSearchResults(query);
+  };
 
   const getMovies = () => {
     if (searchQuery) {
-        dispatch(fetchMovies(`${ENDPOINT_SEARCH}&query=`+searchQuery))
+      dispatch(fetchMovies(`${ENDPOINT_SEARCH}&query=` + searchQuery));
     } else {
-        dispatch(fetchMovies(ENDPOINT_DISCOVER))
+      dispatch(fetchMovies(ENDPOINT_DISCOVER));
     }
-  }
+  };
 
-  const viewTrailer = (movie) => {
-    getMovie(movie.id)
-    if (!videoKey) setOpen(true)
-    setOpen(true)
-  }
+  const viewTrailer = movie => {
+    getMovie(movie.id);
+    if (!videoKey) setOpen(true);
+    setOpen(true);
+  };
 
-  const getMovie = async (id) => {
-    const URL = `${ENDPOINT}/movie/${id}?api_key=${API_KEY}&append_to_response=videos`
+  const getMovie = async id => {
+    const URL = `${ENDPOINT}/movie/${id}?api_key=${API_KEY}&append_to_response=videos`;
 
-    setVideoKey(null)
-    const videoData = await fetch(URL)
-      .then((response) => response.json())
+    setVideoKey(null);
+    const videoData = await fetch(URL).then(response => response.json());
 
     if (videoData.videos && videoData.videos.results.length) {
-      const trailer = videoData.videos.results.find(vid => vid.type === 'Trailer')
-      setVideoKey(trailer ? trailer.key : videoData.videos.results[0].key)
+      const trailer = videoData.videos.results.find(vid => vid.type === 'Trailer');
+      setVideoKey(trailer ? trailer.key : videoData.videos.results[0].key);
     }
-  }
+  };
 
   useEffect(() => {
-    getMovies()
-  }, [])
+    getMovies();
+  }, []);
 
   return (
     <div className="App">
@@ -80,11 +76,11 @@ const App = () => {
 
       <div className="container">
         {videoKey ? (
-          <YouTubePlayer
-            videoKey={videoKey}
-          />
+          <YouTubePlayer videoKey={videoKey} />
         ) : (
-          <div style={{padding: "30px"}}><h6>no trailer available. Try another movie</h6></div>
+          <div style={{padding: '30px'}}>
+            <h6>no trailer available. Try another movie</h6>
+          </div>
         )}
 
         <Routes>
@@ -95,7 +91,7 @@ const App = () => {
         </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
